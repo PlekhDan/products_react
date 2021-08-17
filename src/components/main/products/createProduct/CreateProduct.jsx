8019b95dba4dd0af662247cd404a803a9eed7322
Daria <plekhpochta@gmail.com>
2021-08-17 15:47:54 +0300

    errors form

--- src/components/main/products/createProduct/CreateProduct.jsx
+++ src/components/main/products/createProduct/CreateProduct.jsx
@@ -2,24 +2,7 @@ import React, {Component} from "react";
 import {Button, Form, Input, Message, Select} from "semantic-ui-react";
 
 
-// const MyMessage = () => <div>123</div>
-const MyMessage = () => (
-    <div>
-        <Message
-            error
-            header=''
-            name='id'
-            content='Такой идентификатор уже существует.'
-        />
-        <Message
-            error
-            header=''
-            name='id'
-            content='ошибка, нет группы товала.'
-        />
-    </div>
-
-)
+const MyMessage = (errors) => errors.errors.map(error => <Message error content={error} key={error} />);
 
 export class CreateProduct extends Component {
 
@@ -27,58 +10,57 @@ export class CreateProduct extends Component {
         groups: this.props.data.groups,
         products: this.props.data.products,
         createProduct: {},
-        warning: false,
-        handleMessage: false
+        warningId: false,
+        warningGroup: false,
+        viewErrors: false,
+        errors: []
     }
 
 
     handleSubmit = (event) => {
-        console.log('submit')
-
-        const {products} = this.state;
+        const { products } = this.state;
         event.preventDefault();
         const newProduct = this.state.createProduct;
         console.log(newProduct);
 
         const prod = products.reduce((acc, count) => [...acc, count.id], []).some(el => el === newProduct.id);
 
-        if (prod || !('group' in newProduct)) {
+        const errorId = 'Такой идентификатор уже существует.';
+        const errorGroup = 'Ошибка, нет группы товара.';
+
+        if (prod && !('group' in newProduct)) {
             this.setState({
-                warning: true,
-                handleMessage: true
+                warningId: true,
+                warningGroup: true,
+                viewErrors: true,
+                errors: [errorId, errorGroup]
             })
+        } else if (prod) {
+            this.setState({
+                warningId: true,
+                viewErrors: true,
+                errors: [errorId]
+            })
+        } else if (!('group' in newProduct)) {
+            this.setState({
+                warningGroup: true,
+                viewErrors: true,
+                errors: [errorGroup]
+            })
+        } else {
+            console.log('отправляем и перенаправляем на другую страницу');
         }
 
-        // if (newProduct.id < 0 || prod || newProduct.price <= 0 || newProduct.count <= 0 || !('group' in newProduct)) {
-        //     console.log('ошибка!')
-        //     this.setState({
-        //         warning: (<Message negative>Какие-то ошибки</Message>)
-        //     })
-        // } else {
-        //     console.log('отправляем');
-        // }
-
-        // if (newProduct.id < 0) {
-        //     console.log('id меньше нуля');
-        // } else if (prod) {
-        //     console.log('id существует');
-        // } else if (newProduct.price <= 0) {
-        //     console.log('цена меньше нуля');
-        // } else if (newProduct.count <= 0) {
-        //     console.log('количество товара меньше нуля');
-        // } else if (!('group' in newProduct)) {
-        //     console.log('ошибка, нет группы товала');
-        // } else {
-        //     console.log('отправляем');
-        // }
     }
 
 
     handleChange = (event, {name, value}) => {
         this.setState({
             createProduct: {...this.state.createProduct, [name]: value},
-            warning: false,
-            handleMessage: false
+            warningId: false,
+            warningGroup: false,
+            viewErrors: false,
+            errors: []
         })
     }
 
@@ -87,7 +69,7 @@ export class CreateProduct extends Component {
     }
 
     render() {
-        const {groups} = this.state;
+        const {groups, viewErrors, errors, warningGroup, warningId} = this.state;
         const selects = groups.map(group => ({
             key: group.groupId,
             value: group.name,
@@ -104,7 +86,7 @@ export class CreateProduct extends Component {
                                 placeholder='id'
                                 type='number'
                                 min='0.00'
-                                error={this.state.warning}
+                                error={warningId}
                     />
                     <Form.Field onChange={this.handleChange}
                                 control={Input}
@@ -132,7 +114,7 @@ export class CreateProduct extends Component {
                                 name='group'
                                 label='Группа товара:'
                                 placeholder='group'
-                                error={false}
+                                error={warningGroup}
                     />
                 </Form.Group>
                 <Form.Group widths={2}>
@@ -156,25 +138,15 @@ export class CreateProduct extends Component {
                                 min='0.00'
                                 error={false}
                     />
+
                 </Form.Group>
-                {this.state.handleMessage ? <MyMessage/> : null}
-                {/*<Message*/}
-                {/*    error*/}
-                {/*    header=''*/}
-                {/*    name='id'*/}
-                {/*    content='Такой идентификатор уже существует.'*/}
-                {/*/>*/}
-                {/*<Message*/}
-                {/*    error*/}
-                {/*    header=''*/}
-                {/*    name='group'*/}
-                {/*    content='Добавьте группу товара.'*/}
-                {/*/>*/}
+                <Form.Field>{viewErrors && <MyMessage errors={errors}/>}</Form.Field>
                 <Form.Field
                     control={Button}
                     content='Добавить'
                     type='submit'
                 />
+
             </Form>
         );
     }
